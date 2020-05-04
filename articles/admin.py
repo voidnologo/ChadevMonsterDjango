394ba0cd1a9156a38182639c5b4a0750066dcfd4
voidnologo <(none)>
2020-05-03 21:45:13 -0400

    Articles admin

--- articles/admin.py
+++ articles/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Article
 
 
-admin.site.register(Article)
-
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('title', 'url', 'state', 'created_on')
+    readonly_fields = ('state', 'created_on')
+    list_display = ('title', 'url', 'state')
+    list_filter = ('state',)
